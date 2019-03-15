3e42ee0d9bd712b0e757af66279eaff838b0f004
Tim Krones <tim@opencraft.com>
2019-03-15 15:29:31 +0100

    Replace string "view" argument to url() function with callable.
    Support for string "view" arguments to url() function no longer available starting with Django 1.10.
    
    Cf. https://docs.djangoproject.com/en/2.1/releases/1.10/#features-removed-in-1-10

--- django_lti_tool_provider/tests/urls.py
+++ django_lti_tool_provider/tests/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login
 
 from django_lti_tool_provider import views as lti_views
 
+
 urlpatterns = [
     url(r'', lti_views.LTIView.as_view(), name='home'),
-    url('^accounts/login/$', 'django.contrib.auth.views.login'),
+    url('^accounts/login/$', login),
     url(r'^lti$', lti_views.LTIView.as_view(), name='lti')
 ]
